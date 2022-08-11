e398c88b77986d4a61b836733e707b20a0150409
Juan Ospina <juanospina11@gmail.com>
2022-08-11 13:35:05 -0400

    corrected express function

--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+    // req.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
