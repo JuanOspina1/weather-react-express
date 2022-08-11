5af428d3966f4f12690a4d06428f8feb5b08698f
Juan Ospina <juanospina11@gmail.com>
2022-08-11 14:31:34 -0400

    Fixed server & Heroku deployment - Working!

--- server/index.js
+++ server/index.js
@@ -15,10 +15,6 @@ app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
-  // app.get("*", (req, res) => {
-  //   // req.sendFile(path.resolve(__dirname, "build", "index.html"));
-  //   res.sendFile(path.resolve(__dirname, "../build", "index.html"));
-  // });
 }
 
 ////////////////////////////////////////
